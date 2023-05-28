9607036b8c0e02b6c484f78f778691b638422843
sumitpant190 <114337820+sumitpant190@users.noreply.github.com>
2023-05-28 16:36:09 +0545

    utils completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm/utils/routes/routes.dart';
 import 'package:mvvm/utils/routes/routes_name.dart';
-import 'package:mvvm/view/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm/utils/routes/routes_name.dart';
+import 'package:mvvm/utils/utils.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -11,7 +13,22 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: []),
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Center(
+          child: InkWell(
+            onTap: () {
+              // Navigator.pushNamed(context, RoutesName.home);
+              Utils.toastMessage('Login');
+              Utils.flushBarErrorMessage('message', context);
+            },
+            child: Center(
+                child: Text(
+              'Hello world',
+              style: TextStyle(fontSize: 20),
+            )),
+          ),
+        )
+      ]),
     );
   }
 }
