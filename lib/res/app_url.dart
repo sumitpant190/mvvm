7c47a8c6de4b65178325a29870052c87edd59485
sumitpant190 <114337820+sumitpant190@users.noreply.github.com>
2023-05-29 08:29:15 +0545

    AppUrls completed

---
+++ lib/res/app_url.dart
@@ -0,0 +1,7 @@
+class AppUrl {
+  static var baseUrl = 'http://reqres.in';
+
+  static var loginEndPoint = baseUrl + '/api/login';
+
+  static var registerApiEndPoint = baseUrl + '/api/register';
+}
